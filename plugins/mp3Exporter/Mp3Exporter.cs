6cb35f5daf926e1dcf013ac60e22337288ce4b51
Piotrekol <nara1011@gmail.com>
2018-08-26 13:23:43 +0200

    Add: mp3Exporter plugin
    This most likely won't be in officially distributed SC installer

---
+++ plugins/mp3Exporter/Mp3Exporter.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Windows.Input;
+using ClickCounter;
+
+namespace mp3Exporter
+{
+    public class Mp3Exporter : Mp3ExporterBase
+    {
+        private string baseMp3Location = @"D:\Random\mp3Testing";
+
+        protected override void KeyUp(object sender, RawKeyEventArgs args)
+        {
+            //TODO: hardcoding inbound
+            if (string.IsNullOrEmpty(SongsDirectory) ||
+                args.Key != Key.Insert ||
+                CurrentSearchResult == null ||
+                !CurrentSearchResult.FoundBeatmaps
+                )
+                return;
+
+            var map = CurrentSearchResult.BeatmapsFound[0];
+            var newMp3Name = $"{map.ArtistRoman} - {map.TitleRoman}.mp3";
+
+            var orginalMp3Location = Path.Combine(SongsDirectory, map.Dir, map.Mp3Name);
+            var newMp3Location = Path.Combine(baseMp3Location, newMp3Name);
+
+            if (File.Exists(newMp3Location))
+                return;
+
+            //TODO: mp3 tag editing
+
+            File.Copy(orginalMp3Location, newMp3Location);
+        }
+    }
+}
---
+++ plugins/mp3Exporter/Mp3ExporterBase.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using ClickCounter;
+using StreamCompanionTypes;
+using StreamCompanionTypes.DataTypes;
+using StreamCompanionTypes.Interfaces;
+
+namespace mp3Exporter
+{
+    public abstract class Mp3ExporterBase : IPlugin, IMapDataGetter, IDisposable, ISettings
+    {
+        private readonly SettingNames _names = SettingNames.Instance;
+
+        public bool Started { get; set; }
+        public string Description { get; } = "";
+        public string Name { get; } = nameof(Mp3Exporter);
+        public string Author { get; } = "Piotrekol";
+        public string Url { get; } = "";
+        public string UpdateUrl { get; } = "";
+
+        protected MapSearchResult CurrentSearchResult = null;
+        private ILogger _logger;
+        private KeyboardListener _keyboardListener;
+        private ISettingsHandler _settings;
+        protected string SongsDirectory;
+        public virtual void Start(ILogger logger)
+        {
+            if (Started)
+                return;
+            _logger = logger;
+
+            //find songs folder location
+            var dir = _settings.Get<string>(_names.SongsFolderLocation);
+            if (dir == _names.SongsFolderLocation.Default<string>())
+            {
+                dir = _settings.Get<string>(_names.MainOsuDirectory);
+                dir = Path.Combine(dir, "Songs\\");
+            }
+
+            if (Directory.Exists(dir))
+            {
+                SongsDirectory = dir;
+
+                _keyboardListener = new KeyboardListener();
+                _keyboardListener.KeyUp += KeyUp;
+                _logger?.Log(">Keyboard hooked!", LogLevel.Debug);
+            }
+
+            Started = true;
+        }
+
+        protected abstract void KeyUp(object sender, RawKeyEventArgs args);
+
+        public virtual void SetNewMap(MapSearchResult map)
+        {
+            CurrentSearchResult = map;
+        }
+
+        public void Dispose()
+        {
+            _keyboardListener?.Dispose();
+        }
+
+        public void SetSettingsHandle(ISettingsHandler settings)
+        {
+            _settings = settings;
+        }
+    }
+}
